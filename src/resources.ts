21935709552e6b248a3e9d8be281f5e8a17b4a06
Nicola Da Rold <ndarold@pataviumenergia.it>
2024-09-09 17:24:36 +0200

    change: sample tiles from professional designer

--- src/resources.ts
+++ src/resources.ts
@@ -3,6 +3,9 @@ import sword from './images/sword.png';
 import actorWarrior from './images/actor-warrior.png';
 import grassTileImage from './images/tile-erba.png';
 import waterTileImage from './images/tile-acqua.png';
+import dirtTile from './images/tiles/dirt_128.png';
+import stoneTile from './images/tiles/stone_128.png';
+import planksTile from './images/tiles/planks_128.png';
 
 /**
  * Default global resource dictionary. This gets loaded immediately
@@ -13,6 +16,9 @@ const Resources = {
     ActorWarrior: new ImageSource(actorWarrior),
     GrassTile: new ImageSource(grassTileImage),
     WaterTile: new ImageSource(waterTileImage),
+    DirtTile: new ImageSource(dirtTile),
+    StoneTile: new ImageSource(stoneTile),
+    PlanksTile: new ImageSource(planksTile),
 }
 
 export { Resources }
--- src/scenes/test-isometric/test-isometric.level.ts
+++ src/scenes/test-isometric/test-isometric.level.ts
@@ -1,4 +1,4 @@
-import { Engine, Scene, ImageSource, IsometricMap, Random, vec } from 'excalibur';
+import { Engine, Scene, IsometricMap, Random, vec } from 'excalibur';
 import { Resources } from '@/resources';
 
 /**
@@ -6,28 +6,31 @@ import { Resources } from '@/resources';
  */
 export class TestIsometricLevel extends Scene {
     isoMap: IsometricMap;
-    grassTileSprite;
-    waterTileSprite;
+    dirtTileSprite;
+    stoneTileSprite;
+    planksTileSprite;
 
 
     public async onInitialize(engine: Engine) {
-        this.grassTileSprite = Resources.GrassTile.toSprite();
-        this.waterTileSprite = Resources.WaterTile.toSprite();
+        this.dirtTileSprite = Resources.DirtTile.toSprite();
+        this.stoneTileSprite = Resources.StoneTile.toSprite();
+        this.planksTileSprite = Resources.PlanksTile.toSprite();
         this.isoMap = new IsometricMap({
             pos: vec(250, 10),
-            tileWidth: 64,
-            tileHeight: 32,
-            columns: 15,
-            rows: 15
+            tileWidth: 128,
+            tileHeight: 64,
+            columns: 10,
+            rows: 10
         });
         this.add(this.isoMap);
 
         const rand = new Random(1234)
         for (let tile of this.isoMap.tiles) {
-            if (rand.bool(0.8)) {
-                tile.addGraphic(this.grassTileSprite);
+            // tile.addGraphic(this.planksTileSprite);
+            if (rand.bool(0.7)) {
+                tile.addGraphic(this.dirtTileSprite);
             } else {
-                tile.addGraphic(this.waterTileSprite);
+                tile.addGraphic(rand.bool(0.5) ? this.stoneTileSprite : this.planksTileSprite);
             }
         }
     }
