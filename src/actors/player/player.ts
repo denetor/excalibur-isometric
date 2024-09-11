216b14c47019eaab237230d4ad91a3a63393e16d
Nicola Da Rold <ndarold@pataviumenergia.it>
2024-09-11 14:04:16 +0200

    Add player movement logic with keyboard input handling

--- src/actors/player/player.ts
+++ src/actors/player/player.ts
@@ -1,4 +1,4 @@
-import { Actor, Color, vec } from 'excalibur';
+import {Actor, Color, Engine, Keys, vec} from 'excalibur';
 import { Resources } from '@/resources';
 
 export class Player extends Actor {
@@ -14,7 +14,55 @@ export class Player extends Actor {
   onInitialize() {
     this.graphics.use(Resources.ActorWarrior.toSprite());
     this.z = 9999;
-    this.vel.x = 20;
-    this.vel.y = 10;
+    this.vel.x = 0;
+    this.vel.y = 0;
   }
+
+  /*update(engine: Engine, delta: number) {*/
+  update(engine: Engine, delta: number): void {
+    this.manageKeyboardInput(engine);
+  }
+
+
+  /**
+   * Manages keyboard input to control the movement of the player entity.
+   *
+   * @param {Engine} engine - The game engine that handles input.
+   * @return {void}
+   */
+  manageKeyboardInput(engine: Engine): void {
+    if (engine.input.keyboard.isHeld(Keys.ArrowRight)) {
+      this.moveRight();
+      // this.graphics.use(this.spriteRight);
+    } else if (engine.input.keyboard.isHeld(Keys.ArrowLeft)) {
+      this.moveLeft();
+      // this.graphics.use(this.spriteLeft);
+    }
+    if (engine.input.keyboard.isHeld(Keys.ArrowUp)) {
+      this.moveUp();
+    }
+    if (engine.input.keyboard.isHeld(Keys.ArrowDown)) {
+      this.moveDown();
+    }
+  }
+
+
+  moveRight(): void {
+    this.vel.x = Math.min(50, this.vel.x + 10);
+    this.vel.y = Math.min(50, this.vel.y + 10);
+  }
+  moveLeft(): void {
+    this.vel.x = Math.max(-50, this.vel.x - 10);
+    this.vel.y = Math.min(-50, this.vel.y - 10);
+  }
+  moveDown(): void {
+    this.vel.x = Math.min(-50, this.vel.x - 10);
+    this.vel.y = Math.min(50, this.vel.x + 10);
+  }
+  moveUp(): void {
+    this.vel.x = Math.min(50, this.vel.x + 10);
+    this.vel.y = Math.min(-50, this.vel.y - 10);
+  }
+
+
 }
--- src/resources.ts
+++ src/resources.ts
@@ -1,8 +1,6 @@
 import { ImageSource } from 'excalibur';
 import sword from './images/sword.png';
 import actorWarrior from './images/actor-warrior.png';
-// import grassTileImage from './images/tile-erba.png';
-// import waterTileImage from './images/tile-acqua.png';
 import dirtTile from './images/tiles/dirt_128.png';
 import stoneTile from './images/tiles/stone_128.png';
 import planksTile from './images/tiles/planks_128.png';
@@ -15,8 +13,6 @@ import waterTile from './images/tiles/water_128.png';
 const Resources = {
     Sword: new ImageSource(sword),
     ActorWarrior: new ImageSource(actorWarrior),
-    // GrassTile: new ImageSource(grassTileImage),
-    // WaterTile: new ImageSource(waterTileImage),
     DirtTile: new ImageSource(dirtTile),
     StoneTile: new ImageSource(stoneTile),
     PlanksTile: new ImageSource(planksTile),
