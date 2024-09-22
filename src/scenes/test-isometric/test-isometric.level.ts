ba0a420c965ecf4bdea6f096acf26a82c8b6801c
Nicola Da Rold <nicola.darold@openview.it>
2024-09-22 13:55:11 +0200

    change: replaced terrain tiles with new hand-made ones

--- src/scenes/test-isometric/test-isometric.level.ts
+++ src/scenes/test-isometric/test-isometric.level.ts
@@ -1,47 +1,38 @@
-import {Engine, Scene, IsometricMap, Random, vec, Shape} from 'excalibur';
-import { Resources } from '@/resources';
+import {Engine, Scene, IsometricMap, Random, vec} from 'excalibur';
+import { terrainSpritesheet, spearmanSpritesheet } from '@/resources';
 
 /**
  * Managed scene
  */
 export class TestIsometricLevel extends Scene {
     isoMap: IsometricMap;
-    dirtTileSprite;
-    stoneTileSprite;
-    planksTileSprite;
-    waterTileSprite;
 
 
     public async onInitialize(engine: Engine) {
-        this.dirtTileSprite = Resources.DirtTile.toSprite();
-        this.stoneTileSprite = Resources.StoneTile.toSprite();
-        this.planksTileSprite = Resources.PlanksTile.toSprite();
-        this.waterTileSprite = Resources.WaterTile.toSprite();
         this.isoMap = new IsometricMap({
             pos: vec(250, 10),
-            tileWidth: 128,
-            tileHeight: 64,
+            tileWidth: 64,
+            tileHeight: 32,
             columns: 10,
             rows: 10
         });
         this.add(this.isoMap);
-
         const rand = new Random(1234)
         for (let tile of this.isoMap.tiles) {
-            // tile.addGraphic(this.planksTileSprite);
-            if (rand.bool(0.1)) {
-                // tile with full cell collider
-                tile.addGraphic(this.waterTileSprite);
-                tile.solid = true;
-                tile.addCollider(Shape.Polygon([vec(0, 95), vec(63, -32 + 95), vec(127, 95), vec(63, 32 + 95)]));
-            } else {
-                if (rand.bool(0.6)) {
-                    tile.addGraphic(this.dirtTileSprite);
-                } else {
-                    tile.addGraphic(rand.bool(0.5) ?
-                        this.stoneTileSprite : this.planksTileSprite
-                    );
-                }
+            tile.solid = true;
+            const n = rand.integer(0,10);
+            switch (n) {
+                case 0:
+                    tile.addGraphic(terrainSpritesheet.getSprite(2,0));
+                    break;
+                case 1:
+                    tile.addGraphic(terrainSpritesheet.getSprite(3,0));
+                    break;
+                case 2:
+                    tile.addGraphic(terrainSpritesheet.getSprite(0,1));
+                    break;
+                default:
+                    tile.addGraphic(terrainSpritesheet.getSprite(1,0));
             }
         }
     }
