eb6d2530ba659a6b0aa87910917614b7eeb3abd4
Nicola Da Rold <nicola.darold@openview.it>
2024-09-22 13:34:52 +0200

    add: terrain and spearman tiles 64x64

---
+++ src/actors/units/unit.ts
@@ -0,0 +1,5 @@
+import {Actor} from "excalibur";
+
+export class unit extends Actor {
+
+}
--- src/resources.ts
+++ src/resources.ts
@@ -1,10 +1,13 @@
-import { ImageSource } from 'excalibur';
+import { ImageSource, SpriteSheet } from 'excalibur';
 import sword from './images/sword.png';
 import actorWarrior from './images/actor-warrior.png';
 import dirtTile from './images/tiles/dirt_128.png';
 import stoneTile from './images/tiles/stone_128.png';
 import planksTile from './images/tiles/planks_128.png';
 import waterTile from './images/tiles/water_128.png';
+import terrainTilesSrc from './images/tiles/terrains.spritesheet.png';
+import spearmanTilesSrc from './images/units/spearman.spritesheet.png';
+
 
 /**
  * Default global resource dictionary. This gets loaded immediately
@@ -17,6 +20,31 @@ const Resources = {
     StoneTile: new ImageSource(stoneTile),
     PlanksTile: new ImageSource(planksTile),
     WaterTile: new ImageSource(waterTile),
+    TerrainTiles: new ImageSource(terrainTilesSrc),
+    SpearmanTiles: new ImageSource(spearmanTilesSrc),
 }
 
-export { Resources }
+const terrainSpritesheet = SpriteSheet.fromImageSource({
+    image: Resources.TerrainTiles,
+    grid: {
+        rows: 4,
+        columns: 4,
+        spriteWidth: 64,
+        spriteHeight: 64,
+    }
+});
+const spearmanSpritesheet = SpriteSheet.fromImageSource({
+    image: Resources.SpearmanTiles,
+    grid: {
+        rows: 4,
+        columns: 4,
+        spriteWidth: 64,
+        spriteHeight: 64,
+    }
+});
+
+export {
+    Resources,
+    terrainSpritesheet,
+    spearmanSpritesheet,
+}
